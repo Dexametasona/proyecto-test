d1772aafb5a2034d26eef49738fa111151aba05e
Dexametasona <machicadogomezalexander@gmail.com>
2023-04-15 11:01:40 -0500

    optimized react form

--- src/app/page-test/react-form/react-form.component.ts
+++ src/app/page-test/react-form/react-form.component.ts
@@ -4,25 +4,32 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-react-form',
   templateUrl: './react-form.component.html',
-  styleUrls: ['./react-form.component.scss']
+  styleUrls: ['./react-form.component.scss'],
 })
 export class ReactFormComponent implements OnInit {
-  form= new FormGroup({
-    name: new FormControl('',[Validators.required, Validators.minLength(5)]),
-    pass: new FormControl('',[Validators.required, Validators.minLength(4)])
+  form = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(10),
+      Validators.pattern('') /* regex */,
+    ]),
+    pass: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(10),
+      Validators.pattern(''),
+    ]),
   });
-  alert=false;
-  mensaje='Formulario enviado'
-  constructor() { }
-  consolear(){
-    this.alert=true;
+  alert = false;
+  mensaje = 'Formulario enviado';
+  constructor() {}
+  consolear() {
+    this.alert = true;
 
     setTimeout(() => {
-      this.alert=false;
+      this.alert = false;
     }, 2000);
-    
   }
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
